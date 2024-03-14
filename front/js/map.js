ce99f5c18dabc003e304ffd54f6433284befd4af
Ronan LOUARN <ronan.louarn.dev@gmail.com>
2024-03-14 19:54:11 +0100

    Modification email

--- front/js/map.js
+++ front/js/map.js
@@ -1,12 +1,12 @@
 function initMap() {
-    const map = L.map("map").setView([44.750796946515905, -0.5336631104370813], 16);
+    const map = L.map("map").setView([44.7417832,-0.5329014], 16);
   
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    const marker = L.marker([44.750796946515905, -0.5336631104370813]).addTo(map);
+    const marker = L.marker([44.7417832,-0.5329014]).addTo(map);
 }
 
 initMap();
